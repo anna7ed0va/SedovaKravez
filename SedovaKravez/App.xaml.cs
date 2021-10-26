5008d94b07395ce82f06771adc1452c83bdb19d0
IMac6 <IMac6@DESKTOP-N9EM8FE>
2021-10-26 12:20:32 +0300

    Добавьте файлы проекта.

---
+++ SedovaKravez/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SedovaKravez
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+    class AppData
+    {
+        public static Entities context { get; } = new Entities();
+    }
+}
---
+++ SedovaKravez/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static SedovaKravez.AppData;
+
+
+namespace SedovaKravez
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var Employes = context.Employee.Where(i => i.Phone == loginBox.Text && i.Password == passwordBox.Text).FirstOrDefault();
+            if (Employes != null)
+            {
+                if (Employes == null)
+                {
+                    MessageBox.Show("Пользователь не найден, повторите попытку", "Пользователь не найден", MessageBoxButton.OK);
+                }
+                else if (Employes.Role == 1)
+                {
+                    DoctorWin DocW = new DoctorWin();
+                    DocW.Show();
+                    Application.Current.MainWindow.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Пароль не совпадает повторите попытку", "Ошибка ввода пароля", MessageBoxButton.OK);
+                }
+            }
+        }
+    }
+}
